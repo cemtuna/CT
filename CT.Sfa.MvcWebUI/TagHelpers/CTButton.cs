5c79c71fa33518586ebc7cb9ca32e4f8a22b3c54
cemtuna <cemtuna@gmail.com>
2019-01-23 23:25:56 +0300

    btn

---
+++ CT.Sfa.MvcWebUI/TagHelpers/CTButton.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CT.Sfa.MvcWebUI.TagHelpers
+{
+    [HtmlTargetElement("ct-button")]
+    public class CTButton:TagHelper
+    {
+        private const string ClaimNameUser = "claim-name-user";
+        [HtmlAttributeName(ClaimNameUser)]
+        public string ClaimValueUser { get; set; }
+
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            StringBuilder stringBilder = new StringBuilder();
+
+
+            
+            if (context.AllAttributes.ContainsName(ClaimNameUser))
+            {
+                //check claim
+                
+            }
+
+            stringBilder.AppendFormat("<button>Claim Test</button>");
+
+            output.TagName = "div";
+            output.Content.SetHtmlContent(stringBilder.ToString());
+            
+            return base.ProcessAsync(context, output);
+        }
+
+    }
+}
